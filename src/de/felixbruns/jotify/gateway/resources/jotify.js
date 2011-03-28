e4824f036dc58825ff7e97bb071ee78d8481b990
spotifyonline <spotifyonline@gmail.com>
2011-03-28 21:29:39 +0000

    * Added / fixed toplist support for all available countries.  *

--- src/de/felixbruns/jotify/gateway/resources/jotify.js
+++ src/de/felixbruns/jotify/gateway/resources/jotify.js
@@ -80,7 +80,6 @@ Jotify.prototype.user = function(session, callbacks){
 /* Toplist. */
 Jotify.prototype.toplist = function(params, session, callbacks){
 	params.session = session;
-	
 	this.request('toplist', params, callbacks);
 };
 
@@ -299,4 +298,26 @@ String.prototype.truncate = function(length, suffix){
 	}
 	
 	return this;
+};
+
+
+//implement JSON.stringify serialization
+JSON.stringify = JSON.stringify || function (obj) {
+    var t = typeof (obj);
+    if (t != "object" || obj === null) {
+        // simple data type
+        if (t == "string") obj = '"'+obj+'"';
+        return String(obj);
+    }
+    else {
+        // recurse array or object
+        var n, v, json = [], arr = (obj && obj.constructor == Array);
+        for (n in obj) {
+            v = obj[n]; t = typeof(v);
+            if (t == "string") v = '"'+v+'"';
+            else if (t == "object" && v !== null) v = JSON.stringify(v);
+            json.push((arr ? "" : '"' + n + '":') + String(v));
+        }
+        return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+    }
 };
